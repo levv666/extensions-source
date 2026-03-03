d71023f6cfa1198a1c2f87db851fa74578923335
levv666 <forepicrpg7@gmail.com>
2026-03-03 19:38:02 +0800

    Update NovelCrow.kt

--- src/en/novelcrow/src/eu/kanade/tachiyomi/extension/en/novelcrow/NovelCrow.kt
+++ src/en/novelcrow/src/eu/kanade/tachiyomi/extension/en/novelcrow/NovelCrow.kt
@@ -3,5 +3,14 @@ package eu.kanade.tachiyomi.extension.en.novelcrow
 import eu.kanade.tachiyomi.multisrc.madara.Madara
 
 class NovelCrow : Madara("NovelCrow", "https://novelcrow.com", "en") {
+
     override val useNewChapterEndpoint = true
+
+    override val mangaSubString = ""
+
+    override fun popularMangaRequest(page: Int) =
+        GET(
+                "$baseUrl/page/$page/?s=&post_type=wp-manga&m_orderby=views",
+            headers
+        )
 }
